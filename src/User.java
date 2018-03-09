71986786058d31133338a74e9a75db1423738c2e
letavia <letavia@users.noreply.github.com>
2018-03-09 15:47:17 +0000

    version 0.1

---
+++ src/User.java
@@ -0,0 +1,77 @@
+import javax.swing.JPasswordField;
+
+public class User {
+	private String email;
+	private String username;
+	private JPasswordField password;
+	private enum Specialties {
+	    MUSIC, SINGING, THEATRE, ART, PRODUCT_DESIGN, FASHION_DESIGN,
+	    INTERIOR_DESIGN, GRAPHIC_DESIGN, DANCE, PROGRAMMING, AUDIOVISUAL
+	}
+	public User(String email, String username, JPasswordField password) {
+		super();
+		this.email = email;
+		this.username = username;
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public JPasswordField getPassword() {
+		return password;
+	}
+	public void setPassword(JPasswordField password) {
+		this.password = password;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "User [email=" + email + ", username=" + username + ", password=" + password + "]";
+	}
+
+
+}
+
